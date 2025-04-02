619b259260654c3bd1702e181350c0ab5ae7ee2b
Qurath_Mehdi <qurathmehdi@gmail.com>
2025-04-02 23:09:32 +0530

    done

---
+++ src/main/java/qur/qur/QurApplication.java
@@ -0,0 +1,13 @@
+package qur.qur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QurApplication.class, args);
+	}
+
+}

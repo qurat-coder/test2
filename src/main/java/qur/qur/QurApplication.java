28ee31594f5a1c48437f417d6f8fd96b14bd2c81
qurat-coder <109673753+qurat-coder@users.noreply.github.com>
2025-04-03 13:21:21 +0530

    Update QurApplication.java
    new commit has done

--- src/main/java/qur/qur/QurApplication.java
+++ src/main/java/qur/qur/QurApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 int x=100;
+@configuration
 
 @SpringBootApplication
 public class QurApplication {
